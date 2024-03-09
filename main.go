80600383abe4750fe6dc0a239f06a05ec839aa75
Benjamin Western <code@benjaminwestern.io>
2024-03-09 13:26:01 +1000

    began refactor of the model calls to the sdk

--- main.go
+++ main.go
@@ -3,15 +3,22 @@ package main
 import (
 	"bufio"
 	"bytes"
+	gen "cloud.google.com/go/ai/generativelanguage/apiv1"
+	"context"
 	"encoding/json"
 	"flag"
 	"fmt"
+	"google.golang.org/api/iterator"
 	"io"
 	"log"
 	"net/http"
 	"os"
 	"runtime"
 	"strings"
+
+	pb "cloud.google.com/go/ai/generativelanguage/apiv1/generativelanguagepb"
+
+	"google.golang.org/api/option"
 )
 
 // Documentation URL: https://ai.google.dev/api/rest/v1beta/SafetySetting#HarmBlockThreshold
@@ -21,16 +28,206 @@ const (
 	githubURL       = "https://github.com/benjaminwestern/geminicli"
 )
 
-// API can count the number of tokens in the context and the conversation history
-// curl https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:countTokens?key=$API_KEY \
-//     -H 'Content-Type: application/json' \
-//     -X POST \
-//     -d '{
-//       "contents": [{
-//         "parts":[{
-//           "text": "Write a story about a magic backpack."}]}]}' > response.json
+func getApiKey() (string, option.ClientOption) {
+	token := os.Getenv("API_KEY")
+	if token == "" {
+		log.Fatal("API_KEY is not set")
+		os.Exit(1)
+	}
+
+	clientOptions := option.WithAPIKey(token)
+	return token, clientOptions
+}
+
+func listModels(ctx *context.Context, modelClient *gen.ModelClient) []*pb.Model {
+	modelOptions := pb.ListModelsRequest{}
+	var models []*pb.Model
+	modelIterator := modelClient.ListModels(*ctx, &modelOptions)
+	for {
+		model, err := modelIterator.Next()
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			fmt.Println("failed to list models, please check your settings", err)
+			return nil
+		}
+		models = append(models, model)
+	}
+	return models
+}
+
+func getModel(ctx *context.Context, modelClient *gen.ModelClient, modelId string) (*pb.Model, error) {
+	if !strings.HasPrefix(modelId, "models/"){
+		modelId = "models/" + modelId
+	}
+
+	modelReq := pb.GetModelRequest{
+		Name: modelId,
+	}
+
+	model, err := modelClient.GetModel(*ctx, &modelReq)
+	if err != nil {
+		return nil, err
+	}
+
+	return model, nil
+}
+
+func countTokens(ctx *context.Context, genClient *gen.GenerativeClient, modelId string, contents []*pb.Content) (*pb.CountTokensResponse, error){
+	req := &pb.CountTokensRequest{
+		Model: modelId,
+		Contents: contents,
+	}
+	tokens, err := genClient.CountTokens(*ctx, req)
+	if err != nil {
+		return nil, err
+	}
+	return tokens, nil 
+}
 
 func main() {
+	ctx := context.Background()
+	token, options := getApiKey()
+	modelClient, err := gen.NewModelClient(ctx, options)
+	if err != nil {
+		log.Fatal("failed to create gemini model client")
+	}
+	defer modelClient.Close()
+
+	genClient, err :=  gen.NewGenerativeClient(ctx, options)
+	if err != nil {
+		log.Fatal("failed to create gemini client")
+	}
+	defer genClient.Close()
+
+	modelId := "models/gemini-1.0-pro-latest"
+
+	currentModel, err := getModel(&ctx, modelClient, modelId)
+	if err != nil {
+		fmt.Println("failed to find model", err)
+		return
+	}
+
+	var chats []*pb.Content
+	var parts []*pb.Part
+	userPart := pb.Part{
+		Data: &pb.Part_Text{
+			Text: "how are you gemini?",
+		},	
+	}
+	parts = append(parts, &userPart)
+	chat1 := &pb.Content{
+		Parts: parts,
+		Role: "user",
+	}
+	chats = append(chats, chat1)
+
+	tokens, err := countTokens(&ctx, genClient, modelId, chats)
+	if err != nil {
+		fmt.Println("error counting tokens", err)
+		return
+	}
+
+	if tokens.TotalTokens < currentModel.InputTokenLimit {
+		fmt.Println("input tokens: ", currentModel.InputTokenLimit, " | ",  "current tokens: ", tokens.TotalTokens)
+	}
+
+	TopP := float32(1.0)
+	TopK := int32(1)
+	Temp := float32(0.4)
+	CandidateCount := int32(1)
+
+	var modelSafetySettings []*pb.SafetySetting
+	derogatorySafety := pb.SafetySetting{
+		Category: pb.HarmCategory_HARM_CATEGORY_DEROGATORY,
+		Threshold: pb.SafetySetting_BLOCK_NONE,
+	}
+	modelSafetySettings = append(modelSafetySettings, &derogatorySafety)
+	toxicitySafety := pb.SafetySetting{
+		Category: pb.HarmCategory_HARM_CATEGORY_TOXICITY,
+		Threshold: pb.SafetySetting_BLOCK_NONE,
+	}
+	modelSafetySettings = append(modelSafetySettings, &toxicitySafety)
+	violenceSafety := pb.SafetySetting{
+		Category: pb.HarmCategory_HARM_CATEGORY_VIOLENCE,
+		Threshold: pb.SafetySetting_BLOCK_NONE,
+	}
+	modelSafetySettings = append(modelSafetySettings, &violenceSafety)
+	sexualSafety := pb.SafetySetting{
+		Category: pb.HarmCategory_HARM_CATEGORY_SEXUAL,
+		Threshold: pb.SafetySetting_BLOCK_NONE,
+	}
+	modelSafetySettings = append(modelSafetySettings, &sexualSafety)
+	medicalSafety := pb.SafetySetting{
+		Category: pb.HarmCategory_HARM_CATEGORY_MEDICAL,
+		Threshold: pb.SafetySetting_BLOCK_NONE,
+	}
+	modelSafetySettings = append(modelSafetySettings, &medicalSafety)
+	dangerousSafety := pb.SafetySetting{
+		Category: pb.HarmCategory_HARM_CATEGORY_DANGEROUS,
+		Threshold: pb.SafetySetting_BLOCK_NONE,
+	}
+	modelSafetySettings = append(modelSafetySettings, &dangerousSafety)
+	harassmentSafety := pb.SafetySetting{
+		Category: pb.HarmCategory_HARM_CATEGORY_HARASSMENT,
+		Threshold: pb.SafetySetting_BLOCK_NONE,
+	}
+	modelSafetySettings = append(modelSafetySettings, &harassmentSafety)
+	hateSpeechSafety := pb.SafetySetting{
+		Category: pb.HarmCategory_HARM_CATEGORY_HATE_SPEECH,
+		Threshold: pb.SafetySetting_BLOCK_NONE,
+	}
+	modelSafetySettings = append(modelSafetySettings, &hateSpeechSafety)
+	sexuallyExplicitSafety := pb.SafetySetting{
+		Category: pb.HarmCategory_HARM_CATEGORY_SEXUALLY_EXPLICIT,
+		Threshold: pb.SafetySetting_BLOCK_NONE,
+	}
+	modelSafetySettings = append(modelSafetySettings, &sexuallyExplicitSafety)
+	dangerousContentSafety := pb.SafetySetting{
+		Category: pb.HarmCategory_HARM_CATEGORY_DANGEROUS_CONTENT,
+		Threshold: pb.SafetySetting_BLOCK_NONE,
+	}
+	modelSafetySettings = append(modelSafetySettings, &dangerousContentSafety)
+
+	modelConfig := pb.GenerationConfig{
+		TopP: &TopP,
+		TopK: &TopK,
+		Temperature: &Temp,
+		StopSequences: []string{},
+		MaxOutputTokens: &currentModel.OutputTokenLimit,
+		CandidateCount: &CandidateCount,
+	}
+
+	modelRequest := pb.GenerateContentRequest{
+		Model: currentModel.Name,
+		Contents: chats,
+		SafetySettings: modelSafetySettings,
+		GenerationConfig: &modelConfig,
+	}
+
+
+	resp, err := genClient.GenerateContent(ctx, &modelRequest)
+	if err != nil {
+		fmt.Println("failed to get request from model", err)
+	}
+
+	fmt.Println(resp)
+
+
+
+	// model, err := getModel(&ctx, modelClient, "gemini-1.0-pro-latest")
+	// if err != nil {
+	// 	fmt.Println("failed to find model", err)
+	// }
+	//
+	// fmt.Println(model)
+	//
+	// models := listModels(&ctx, modelClient)
+	// for _, model := range models {
+	// 	fmt.Println(model.DisplayName, "|", strings.Split(model.Name, "/")[1])
+	// }
+
 	conversationHistory := []Content{}
 	contextFile := flag.String("context", "", "Path to the context file")
 	outputPath := flag.String("output", "", "Path to the output file")
@@ -50,12 +247,6 @@ func main() {
 	if *help {
 		flag.Usage()
 		os.Exit(0)
-	}
-
-	token := os.Getenv("API_KEY")
-	if token == "" {
-		log.Fatal("API_KEY is not set")
-		os.Exit(1)
 	}
 
 	outputTokenLimit := parseInt(importEnvironmentVariables("MAX_OUTPUT_TOKENS", "2048", *debug), *debug)
@@ -103,7 +294,6 @@ func main() {
 	apiVersion := importEnvironmentVariables("API_VERSION", "v1beta", *debug)
 
 	url := fmt.Sprintf("%s/%s/models/%s:generateContent", baseURL, apiVersion, modelType)
-
 	geminiURL := fmt.Sprintf("%s?key=%s", url, token)
 
 	logFile, err := createTimestampedLogFile(*outputPath)
